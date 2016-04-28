5d757e20f9d05813a838c2a8bb942cd8982537a9
Anne Martus <s58843@beuth-hochschule.de>
2016-04-28 19:34:39 +0200

    Anne changed the version #

--- src/liv/liv.java
+++ src/liv/liv.java
@@ -4,11 +4,11 @@
 package liv;
 
 /**
- * Project: Liv � Lebensmittelinhaltverifizierer
+ * Project: Liv - Lebensmittelinhaltverifizierer
  * 
  * @author team equal-IT
  * mail: team@equal-it.de
- * @version  00.00.01 2016/04/28
+ * @version  00.00.03 2016/04/28 - 19:33
  */
 public class liv {
 	
@@ -18,7 +18,7 @@ public class liv {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello, I´am LIV - Lebensmittelinhaltverifizierer.");
+		System.out.println("Hello, I am LIV - Lebensmittelinhaltverifizierer.");
 
 	}
 
