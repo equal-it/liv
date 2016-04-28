ad2bd224db3a060ebf8d7ced47baf2255d877581
felixwyrwal <felixwyrwal@gmx.de>
2016-04-28 18:43:34 +0200

    initial liv commit

---
+++ src/liv/liv.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package liv;
+
+/**
+ * @author felix
+ *
+ */
+public class liv {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
