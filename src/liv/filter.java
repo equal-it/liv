bff6bf975f73631a675b51c06e23a6a0807da396
felix <felix@Felix-T540p.fritz.box>
2016-04-29 10:51:54 +0200

    initial commit filter.class
    add method setFilter getFilter

---
+++ src/liv/filter.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package liv;
+
+/**
+ * class filter
+ * 
+ * Project: Liv - Lebensmittelinhaltverifizierer
+ * 
+ * @author team equal-IT
+ * mail: team@equal-it.de
+ * @version  00.00.01 2016/04/29 - 10:47
+ *
+ */
+
+public class filter {
+
+	int[] filter = new int[4];
+
+	public void  setFilter(int a, int b, int c, int d, int e) {
+		filter[0] = a;
+		filter[1] = b;
+		filter[2] = c;
+		filter[3] = d;
+		filter[4] = e;
+	}
+	
+	public int[] getFilter(){
+		return  filter;
+	}
+
+}
