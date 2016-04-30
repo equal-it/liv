c13d2ae6cd96e8ae6fed775ff8276cae66eaef06
Luke2 <Luke2@Luke2PC.Speedport_W_724V_Typ_A_05011603_00_009>
2016-04-30 11:15:57 +0200

    jdbc und http abfragen für laktonaut

---
+++ src/liv/HttpAbfrage.java
@@ -0,0 +1,34 @@
+package liv;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+//import javax.net.ssl.HttpsURLConnection;
+
+public class HttpAbfrage {
+	private final String USER_AGENT = "Mozilla/5.0";
+	public static void main (String[] args) throws Exception {
+		HttpAbfrage http = new HttpAbfrage();
+		System.out.println("test - send http get request");
+		http.sendGet();
+	}
+		//http get request
+		private void sendGet() throws Exception {
+		String url = "http://www.laktonaut.de/";
+		URL obj = new URL (url);
+		HttpAbfrage con=(HttpAbfrage) obj.openConnection();
+		con.setRequestMethod("GET");
+		con.setRquestProperty("User-Agent", USER_AGENT);
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+		BufferedReader in = new StringBuffer();
+		while ((inputLine = in.readline()) !=null) {
+			response.append(inputLine);			
+			}
+		in.close();
+		
+	}
+}
