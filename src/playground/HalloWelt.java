5cb0dbd3a1795fa21176c806224a51440b05c6a1
Luke2 <Luke2@Luke2PC.Speedport_W_724V_Typ_A_05011603_00_009>
2016-05-04 22:16:22 +0200

    hallo welt

---
+++ src/playground/HalloWelt.java
@@ -0,0 +1,13 @@
+package playground;
+
+public class HalloWelt {
+
+	public static void main(String[] args) {
+				System.out.println("Das ist die erste Zeile!");
+				System.out.print("Anfang der zweiten Zeile ");
+				System.out.println(" und Fortsetzung der zweiten Zeile");
+				System.out.println("Das ist die letzte Zeile!");
+
+	}
+
+}
