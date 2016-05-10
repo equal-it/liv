d3d5f74688427ddf29592f94da19b97594d2d17a
Anne Martus <s58843@beuth-hochschule.de>
2016-05-10 21:47:39 +0200

    Neue Klasse VergleichFilter angelegt

---
+++ src/liv/VergleichFilter.java
@@ -0,0 +1,25 @@
+package liv;
+
+/**
+ * Project: Liv - Lebensmittelinhaltverifizierer
+ * 
+ * class VergleichFilter
+ * Klasse vergleicht gesetzten Filter mit zurueckgegebenen Werten aus der DB
+ * 
+ * @author team equal-IT
+ * @mail: team@equal-it.de
+ * @version 00.00.03 2016/05/10
+ */
+
+
+public class VergleichFilter {
+	
+	// muss aktuell gesetzten Filter aus der Klasse Filter holen (getFilter)
+	// muss Daten aus HttpAbfrageLaktonaut holen ob yes oder no (isLaktose)
+	// dann vergleichen, ob wenn Filter 01 = true und isLaktose = yes dann setze
+	// Ampel auf rot, wenn unbekannt dann gelb, wenn no dann gruen
+	
+	// Reihenfolge: Filter muss gesetzt sein, HttpAbfrage muss aufgerufen sein, Barcodeeingabe
+	// muss passiert sein
+
+}
