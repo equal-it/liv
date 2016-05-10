6edf55a71ed5ce73113f825dd6db2b6cad1ade62
Anne Martus <s58843@beuth-hochschule.de>
2016-05-10 20:28:01 +0200

    Header in HttpAbfrageDatakick verbessert, EingabeEAN angelegt

---
+++ src/liv/EingabeEAN.java
@@ -0,0 +1,17 @@
+package liv;
+
+/**
+ * class Filter
+ * 
+ * Project: Liv - Lebensmittelinhaltverifizierer
+ * 
+ * @author team equal-IT, team@equal-it.de
+ * @version 00.00.01 2016/05/10
+ *
+ */
+
+public class EingabeEAN {
+	
+	// Hier muessen Methoden hin, die einen per Konsole eingegebenen EAN einlesen
+
+}
--- src/playground/HttpAbfrageDatakick.java
+++ src/playground/HttpAbfrageDatakick.java
@@ -1,6 +1,15 @@
 package playground;
 
-// Anne
+/**
+ * class HttpAbfrageDatakick
+ * 
+ * Project: Liv - Lebensmittelinhaltverifizierer
+ * 
+ * @author Anne, anne@equal-it.de
+ * @version 00.00.01 2016/05/10
+ *
+ */
+
 
 public class HttpAbfrageDatakick {
 	
