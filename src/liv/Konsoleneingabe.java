50687e02c63354e5d8c8cea2e06c74cf9548ad25
felixwyrwal <felixwyrwal@gmx.de>
2016-05-30 19:41:51 +0200

    delete unused import

--- src/liv/Konsoleneingabe.java
+++ src/liv/Konsoleneingabe.java
@@ -3,7 +3,6 @@ package liv;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Collection;
 
 public class Konsoleneingabe {
