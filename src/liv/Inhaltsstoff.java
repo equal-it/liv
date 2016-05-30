400167becc50e6c225b16bb46ff16e89dbe16386
felixwyrwal <felixwyrwal@gmx.de>
2016-05-30 19:26:39 +0200

    main aus prüfeEAN und Inhaltsstoff gelöscht

--- src/liv/Inhaltsstoff.java
+++ src/liv/Inhaltsstoff.java
@@ -19,21 +19,6 @@ public enum Inhaltsstoff {
 		return this.anzeigename;
 	}
 
-	public static void main(String[] args) {
-		System.out.println("\nWelchen Filter wollen Sie setzen?\n");
-		for (Inhaltsstoff stoff : Inhaltsstoff.values()) {
-			if (stoff != UNBEKANNT)
-				System.out.println("Waehle " + stoff.code + " fuer " + stoff.anzeigename + ".");
-		}
-		System.out.println("\nOder mit 'H' zum Hauptmenue. \n");
-
-		String antwort = "dfkfgsdl gjfkdlgfk xml jfio <lactose>no</lactose>  fhuiw 7879 hjfejk";
-		String[] schritt1 = antwort.split("<lactose>");
-		String[] schritt2 = schritt1[1].split("</lactose>");
-		String ergebnis = schritt2[0];
-		System.out.println("Antwort enthaelt Laktose: " + ergebnis);
-	}
-
 	public static Inhaltsstoff inhaltstoffFuerCode(final String code) {
 		for (Inhaltsstoff inhaltsstoff : values()) {
 			if (inhaltsstoff.code.equals(code)) {
--- src/liv/Konsoleneingabe.java
+++ src/liv/Konsoleneingabe.java
@@ -7,17 +7,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 
 public class Konsoleneingabe {
-
-	public static void main(String[] args) {
-
-		Collection<String> gueltigeWerte = new ArrayList<>();
-		gueltigeWerte.add("a");
-		gueltigeWerte.add("2");
-		gueltigeWerte.add("Q");
-		String ergebnis = Konsoleneingabe.leseKonsoleFuer(gueltigeWerte);
-		System.out.println(ergebnis);
-	}
-
+	
 	public static String leseKonsoleFuer(Collection<String> gueltigeWerte) {
 		String eingabe = "";
 		InputStreamReader inReader = new InputStreamReader(System.in);
