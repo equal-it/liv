d894b8ea40aec64c92310f7aa036d9a7a8006f80
felixwyrwal <felixwyrwal@gmx.de>
2016-06-01 19:48:13 +0200

    add author

--- src/gui/WindowListener.java
+++ src/gui/WindowListener.java
@@ -2,7 +2,11 @@ package gui;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
+/**
+ * 
+ * @author felix
+ *
+ */
 class WindowListener extends WindowAdapter {
 
 	public void windowClosing(WindowEvent e) {
