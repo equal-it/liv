b405cb9be1c271457487278ce8090bfad782aba4
ani <s58644@beuth-hochschule.de>
2016-06-14 20:54:12 +0200

    bug fixed

--- src/liv/VergleichFilter.java
+++ src/liv/VergleichFilter.java
@@ -23,12 +23,15 @@ public class VergleichFilter {
 	 */
 	public static String ueberprufeIndikatoren(List<Ampelindikator> ergebnisse) {
 
-		String isProductOK = "green";
+		String isProductOK = "";
 
 		if (ergebnisse.isEmpty() || ergebnisse.contains(Ampelindikator.UNBEKANNT)) {
 			isProductOK = "yellow";
 		} if (ergebnisse.contains(Ampelindikator.ENTHALTEN)) {
 			isProductOK = "red";
+		}
+		if (ergebnisse.contains(Ampelindikator.NICHT_ENTHALTEN)) {
+			isProductOK = "green";
 		}
 		return isProductOK;
 	}
