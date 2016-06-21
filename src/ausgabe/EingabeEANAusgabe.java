d508d43d24802735ae6836bf6b279b44ec1b4c1c
felixwyrwal <felixwyrwal@gmx.de>
2016-06-21 20:06:25 +0200

    ausgabe test ean angepasst

--- src/ausgabe/EingabeEANAusgabe.java
+++ src/ausgabe/EingabeEANAusgabe.java
@@ -3,16 +3,16 @@ package ausgabe;
 public class EingabeEANAusgabe {
 
 	public static void testEANAuagabe() {
-		System.out.println("--------------------------------------------");
-		System.out.println("\nBitte (Test-)EAN eingeben:\n");
-		System.out.println("Test EAN (Ritter Sport Marzipan 100 g) Laktose = no: 4000417025005 (in LIV DB)\n"
-				+ "Test EAN (Ferrero Nutella 1000 g) Laktose = yes: 4008400401829  (in LIV DB)\n"
-				+ "Test EAN (unbekanntes Produkt) Laktose = unbekannt: 4008400401928 \n"
-				+ "Test EAN (Griesson Softcakes) Laktose = no: 4001518722466 (in LIV DB)\n"
-				+ "Test EAN (Ferrero Giotto) Laktose = yes: 4008400121925 \n"
-				+ "Test EAN (Rapunzel Gemüsebrühe) Laktose = no: 4006040271505 \n"
-				+ "Test EAN (Ritter Sport Dunkle Vollnuss) Laktose = yes: 4000417224002 \n"
-				+ "Test EAN (Alnatura Dinkel Doppel Keks 330g) Laktose = yes: 4104420053922 (in LIV DB)\n" + "");
+		System.out.println("\nBitte (Test-)EAN eingeben:");
+		System.out.println(
+				"\nTest EAN (Ritter Sport Marzipan 100 g) Laktose = no / Gluten = no / Nuss = no: 4000417025005 (in LIV DB)\n"
+						+ "Test EAN (Ferrero Nutella 1000 g) Laktose = yes / Gluten = no / Nuss = yes = yes: 4008400401829  (in LIV DB)\n"
+						+ "Test EAN (Griesson Softcakes) Laktose = no / Gluten = no / Nuss = no: 4001518722466 (in LIV DB)\n"
+						+ "Test EAN (Alnatura Dinkel Doppel Keks 330g) Laktose = yes / Gluten = no / Nuss = no: 4104420053922 (in LIV DB)\n"
+						+ "Test EAN (unbekanntes Produkt) Laktose = unbekannt: 4008400401928 \n"
+						+ "Test EAN (Ferrero Giotto) Laktose = yes: 4008400121925 \n"
+						+ "Test EAN (Rapunzel Gemüsebrühe) Laktose = no: 4006040271505 \n"
+						+ "Test EAN (Ritter Sport Dunkle Vollnuss) Laktose = yes: 4000417224002 \n");
 		System.out.println("--------------------------------------------");
 	}
 
