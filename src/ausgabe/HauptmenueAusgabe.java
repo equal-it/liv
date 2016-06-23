c6aad4c35430e0ca3eb6bf1618585f848c032d6c
mel <s58400@beuth-hochschule.de>
2016-06-23 18:57:21 +0200

    Hauptmenüeintrag korrigiert

--- src/ausgabe/HauptmenueAusgabe.java
+++ src/ausgabe/HauptmenueAusgabe.java
@@ -7,7 +7,7 @@ public class HauptmenueAusgabe {
 	public static void LivHauptmenueAusgabe() {
 		System.out.println("\n--------------------------------------------");
 		System.out.println(FILTER.code() + " - Filter setzten");
-		System.out.println(EAN.code() + " - EAN anzeigen");
+		System.out.println(EAN.code() + " - EAN eingeben");
 		System.out.println(IMPRESSUM.code() + " - Impressum");
 		System.out.println(ENDE.code() + " - Beenden");
 		System.out.println("--------------------------------------------");
