2b8c3b4c5df1a7cf114706176cadc1247c5a5724
anne <anne.martus@web.de>
2016-07-01 08:52:03 +0200

    Menüanpassung

--- src/ausgabe/HauptmenueAusgabe.java
+++ src/ausgabe/HauptmenueAusgabe.java
@@ -1,13 +1,16 @@
 package ausgabe;
 
-import static ausgabe.HauptmenueEintraege.*;
+import static ausgabe.HauptmenueEintraege.EAN;
+import static ausgabe.HauptmenueEintraege.ENDE;
+import static ausgabe.HauptmenueEintraege.FILTER;
+import static ausgabe.HauptmenueEintraege.IMPRESSUM;
 
 /**
  * Project: LIV - Lebensmittelinhaltsstoffverifizierer
  * 
  * class HauptmenueAusgabe
  * 
- * @author team equal-IT 
+ * @author team equal-IT
  * @mail: team@equal-it.de
  * @version 00.00.10 2016/05/16
  */
@@ -16,7 +19,7 @@ public class HauptmenueAusgabe {
 
 	public static void LivHauptmenueAusgabe() {
 		System.out.println("\n--------------------------------------------");
-		System.out.println ("HAUPTMENUE");
+		System.out.println("HAUPTMENUE");
 		System.out.println("--------------------------------------------\n");
 		System.out.println(FILTER.code() + " - Filter setzten");
 		System.out.println(EAN.code() + " - EAN eingeben");
@@ -27,21 +30,23 @@ public class HauptmenueAusgabe {
 	}
 
 	public static void eingabeMenueAndereDB() {
-		System.out.println ("\nDas Produkt ist nicht in der LIV-Datenbank enthalten!!!\n");
+		System.out
+				.println("\nDas Produkt ist nicht in der LIV-Datenbank enthalten!!!\n");
 		System.out.println("\n--------------------------------------------");
-		System.out.println ("xxxMENUE"); //Klasse HauptmenueAusgabe
+		System.out.println("MENUE EXT DB"); // Klasse HauptmenueAusgabe
 		System.out.println("--------------------------------------------\n");
-		System.out.println ("\n1 - Externe Datenbanken durchsuchen" + 
-							"\n2 - Produkt zur LIV-Datenbank hinzuefuegen" + 
-							"\n3 - Abbrechen und zurueck zum Hauptmenue");
-	
-	
+		System.out.println("\n1 - Externe Datenbanken durchsuchen"
+				+ "\n2 - Produkt zur LIV-Datenbank hinzuefuegen"
+				+ "\n3 - Abbrechen und zurueck zum Hauptmenue");
+
 	}
-	public static void eingabeMenueInLivDBEintragen(){
+
+	public static void eingabeMenueInLivDBEintragen() {
 		System.out.println("\n--------------------------------------------");
-		System.out.println ("xxxMENUE"); //Klasse HauptmenueAusgabe
+		System.out.println("MENUE LIV DB"); // Klasse HauptmenueAusgabe
 		System.out.println("--------------------------------------------\n");
-		System.out.println("\n1 - Produkt zur LIV-Datenbank hinzufuegen\n" + "2 - Zurueck zum Hauptmenue");
-		
+		System.out.println("\n1 - Produkt zur LIV-Datenbank hinzufuegen\n"
+				+ "2 - Zurueck zum Hauptmenue");
+
 	}
 }
